eb40044e8c2d52d3424d24275e00f73267817934
Sean Donegan <spdonegan1@gmail.com>
2013-02-27 20:30:32 -0500

    Do some calendar stuff

--- static/js/interactions.js
+++ static/js/interactions.js
@@ -43,6 +43,24 @@ $(document).ready(function(){
         makeCalendar(calendarDate);
     });
 
+    $(".cal").hover(function(){
+        // $(this).append("<div id='popup'>nksegeglsnk</div>");
+        var id = $(this).attr('id');
+        var foo = id.split("_");
+        var row = foo[1];
+        var col = foo[2];
+        var startCol = calendarDate.getDay();
+        console.log(startCol);
+        console.log(row);
+        if ((row === "1") && (parseInt(col) < startCol+1)){
+            return;
+        }
+        console.log(foo);
+        // console.log(calendarDate);
+    },
+    function () {
+    $(this).find("#popup").remove();
+  });
      makeCalendar(calendarDate);
 
 });
